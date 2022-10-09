01721f1c6bb03811456105df686ee3b243185a09
WalkinBurger <nchunghim@gmail.com>
2022-10-09 13:39:28 +0100

    e

---
+++ assets/languages/zh_CN.py
@@ -0,0 +1,11 @@
+class zhCN:
+    # Language window
+    text_langSelect = "选择你的语言"
+    text_langContinue = "按此继续"
+    # Endings
+    text_meltdown = "熔毁"
+    text_meltdownDES = "发电机温度超出了可承受的极限(4000°C)！整座\n设施发生了一场巨大的爆炸，并且摧毁了\n整个设施以及周边的环境。\n \n提示：记得检查冷冻液是否开启，以及是否有管道破裂。"
+    text_freezedown = "冻毁"
+    text_freezedownDES = "由于温度过低(-4000°C)，一个黑洞产生了，并\n摧毁了整个设施，灾害甚至蔓延到整个\n世界。\n“游戏结束了，兄弟！游戏结束！”\n \n提示：记得随时检查两个发电机的温度，并在适当时机关闭或开启冷冻液。"
+    text_blackout = "设施停电"
+    text_blackoutDES = "由于发电机的能源用尽，没有能量继续泵动颜料，\n设施无法再继续产生电力，游戏结束。\n \n提示：记得及时维修运输燃料的管道！"
