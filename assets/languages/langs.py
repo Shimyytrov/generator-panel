9ad46d6899201ec9319eeb2ac44c96edd4b26abe
YamiyoshinLevi <81023608+YamiyoshinLevi@users.noreply.github.com>
2022-10-09 21:45:41 +0800

    1

--- assets/languages/langs.py
+++ assets/languages/langs.py
@@ -6,7 +6,7 @@ class zhCN:
     text_meltdown = ["熔毁"]
     text_meltdownDES = ["发电机温度超出了可承受的极限(4000°C)！整座 ", "设施发生了一场巨大的爆炸，并且摧毁了 ", "整个设施以及周边的环境。 ", "提示：记得检查冷冻液是否开启，以及是否有管道破裂。"]
     text_freezedown = ["冻毁"]
-    text_freezedownDES = ["由于温度过低(-4000°C)，一个黑洞产生了，并 ", "摧毁了整个设施，灾害甚至蔓延到整个 ", "世界。 ", "“游戏结束了，兄弟！游戏结束！” ", "提示：记得随时检查两个发电机的温度，并在适当时机关闭或开启冷冻液。"]
+    text_freezedownDES = ["由于温度过低(-4000°C)，一个黑洞产生了，并 ", "摧毁了整个设施，灾害甚至蔓延到整个 ", "世界。 ", "“游戏结束，伙计，游戏结束！” ", "提示：记得随时检查两个发电机的温度，并在适当时机关闭或开启冷冻液。"]
     text_blackout = ["设施停电"]
     text_blackoutDES = ["由于发电机的能源用尽，没有能量继续泵动颜料， ", "设施无法再继续产生电力，游戏结束。 ", "提示：记得及时维修运输燃料的管道！"]
 
@@ -18,30 +18,30 @@ class zhTW:
     text_meltdown = ["熔毀"]
     text_meltdownDES = ["發電機溫度超出了可承受的極限(4000°C)！整座 ", "設施發生了一場巨大的爆炸，並且摧毀了 ", "整個設施以及周邊的環境。 ", "提示：記得檢查冷凍液是否開啟，以及是否有管道破裂。"]
     text_freezedown = ["凍毀"]
-    text_freezedownDES = ["由於溫度過低(-4000°C)，一個黑洞產生了，並 ", "摧毀了整個設施，災害甚至蔓延到整個 ", "世界。 ", "“遊戲結束了，兄弟！遊戲結束！” ", "提示：記得隨時檢查兩個發電機的溫度，並在適當時機關閉或開啟冷凍液。"]
+    text_freezedownDES = ["由於溫度過低(-4000°C)，一個黑洞產生了，並 ", "摧毀了整個設施，災害甚至蔓延到整個 ", "世界。 ", "“遊戲結束，夥計，遊戲結束！” ", "提示：記得隨時檢查兩個發電機的溫度，並在適當時機關閉或開啟冷凍液。"]
     text_blackout = ["設施停電"]
     text_blackoutDES = ["由於發電機的能源用盡，沒有能量繼續泵動顏料， ", "設施無法再繼續產生電力，遊戲結束。 ", "提示：記得及時維修運輸燃料的管道！"]
 
 class EN:
     # Language window
-    text_langSelect = ["选择你的语言"]
-    text_langContinue = ["按此继续"]
+    text_langSelect = ["Select Your Language"]
+    text_langContinue = ["Continue"]
     # Endings
-    text_meltdown = ["熔毁"]
-    text_meltdownDES = ["发电机温度超出了可承受的极限(4000°C)！整座 ", "设施发生了一场巨大的爆炸，并且摧毁了 ", "整个设施以及周边的环境。 ", "提示：记得检查冷冻液是否开启，以及是否有管道破裂。"]
-    text_freezedown = ["冻毁"]
-    text_freezedownDES = ["由于温度过低(-4000°C)，一个黑洞产生了，并 ", "摧毁了整个设施，灾害甚至蔓延到整个 ", "世界。 ", "“游戏结束了，兄弟！游戏结束！” ", "提示：记得随时检查两个发电机的温度，并在适当时机关闭或开启冷冻液。"]
-    text_blackout = ["设施停电"]
-    text_blackoutDES = ["由于发电机的能源用尽，没有能量继续泵动颜料， ", "设施无法再继续产生电力，游戏结束。 ", "提示：记得及时维修运输燃料的管道！"]
+    text_meltdown = ["Meltdown"]
+    text_meltdownDES = ["The generator temperature is beyond the acceptable limit(4000°C)!", "A huge explosion occurred in the whole facility. For the result,", "the facility and the surrounding have been destroyed.", "Hint: Remember to check if the coolant is activated and if there are no broken pipes."]
+    text_freezedown = ["Freezedown"]
+    text_freezedownDES = ["Due to the low temperature(-4000°C), a black hole was created,", "and destroyed the whole facility. The disaster even spread to the whole world.", "“Game over, man, game over!” ", "Hint: Remember to check the temperature of both generators at all times,", "and turn off or turn on the coolant when appropriate."]
+    text_blackout = ["Facility-wide Blackout"]
+    text_blackoutDES = ["With the generator running out of energy, there is no energy to continue pumping the paint,", "Game over.", "Hint: Remember to repair the pipes that transport fuel!"]
 
 class DE:
     # Language window
-    text_langSelect = ["選擇你的語言"]
-    text_langContinue = ["按此繼續"]
+    text_langSelect = ["Wähle deine Sprache"]
+    text_langContinue = ["Fortsetzen"]
     # Endings
-    text_meltdown = ["熔毀"]
-    text_meltdownDES = ["發電機溫度超出了可承受的極限(4000°C)！整座 ", "設施發生了一場巨大的爆炸，並且摧毀了 ", "整個設施以及周邊的環境。 ", "提示：記得檢查冷凍液是否開啟，以及是否有管道破裂。"]
-    text_freezedown = ["凍毀"]
-    text_freezedownDES = ["由於溫度過低(-4000°C)，一個黑洞產生了，並 ", "摧毀了整個設施，災害甚至蔓延到整個 ", "世界。 ", "“遊戲結束了，兄弟！遊戲結束！” ", "提示：記得隨時檢查兩個發電機的溫度，並在適當時機關閉或開啟冷凍液。"]
-    text_blackout = ["設施停電"]
-    text_blackoutDES = ["由於發電機的能源用盡，沒有能量繼續泵動顏料， ", "設施無法再繼續產生電力，遊戲結束。 ", "提示：記得及時維修運輸燃料的管道！"]
+    text_meltdown = ["Kernschmelze"]
+    text_meltdownDES = ["Die Generatortemperatur liegt außerhalb der zulässigen Grenze(4000 °C)!", "In der gesamten Anlage ereignete sich eine riesige Explosion.", "Dafür wurden die Anlage und die Umgebung zerstört.", "Hinweis: Denken Sie daran, zu prüfen, ob das Kühlmittel", "aktiviert ist und ob es keine gebrochenen Rohre gibt."]
+    text_freezedown = ["Kerneinfriere(Kern Einfrieren)"]
+    text_freezedownDES = ["Aufgrund der niedrigen Temperatur(-4000 ° C) wurde ein Schwarzes Loch geschaffen,", "Es zerstörte die ganze Anlage. Die Katastrophe breitete sich sogar auf", "der ganzen Welt aus.", "“Game over, mann, game over!” ", "Hinweis: Denken Sie daran, die Temperatur beider Generatoren jederzeit", "zu überprüfen und gegebenenfalls das Kühlmittel aus- oder einzuschalten."]
+    text_blackout = ["Anlagenweiter Blackout"]
+    text_blackoutDES = ["Wenn der Generator keine Energie mehr hat, gibt es keine Energie, um die Farbe weiter zu pumpen,", "Spiel vorbei.", "Hinweis: Denken Sie daran, die Rohre zu reparieren, die Kraftstoff transportieren!"]
