59aa26462419059fab912259e56b792bd613f78c
YamiyoshinLevi <81023608+YamiyoshinLevi@users.noreply.github.com>
2022-10-09 20:50:08 +0800

    Update main.pyw

--- assets/languages/langs.py
+++ assets/languages/langs.py
@@ -12,12 +12,12 @@ class zhCN:
 
 class zhTW:
     # Language window
-    text_langSelect = "选择你的语言"
-    text_langContinue = "按此继续"
+    text_langSelect = "選擇你的語言"
+    text_langContinue = "按此繼續"
     # Endings
-    text_meltdown = "熔毁"
-    text_meltdownDES = "发电机温度超出了可承受的极限(4000°C)！整座\n设施发生了一场巨大的爆炸，并且摧毁了\n整个设施以及周边的环境。\n \n提示：记得检查冷冻液是否开启，以及是否有管道破裂。"
-    text_freezedown = "冻毁"
-    text_freezedownDES = "由于温度过低(-4000°C)，一个黑洞产生了，并\n摧毁了整个设施，灾害甚至蔓延到整个\n世界。\n“游戏结束了，兄弟！游戏结束！”\n \n提示：记得随时检查两个发电机的温度，并在适当时机关闭或开启冷冻液。"
-    text_blackout = "设施停电"
-    text_blackoutDES = "由于发电机的能源用尽，没有能量继续泵动颜料，\n设施无法再继续产生电力，游戏结束。\n \n提示：记得及时维修运输燃料的管道！"
+    text_meltdown = "熔毀"
+    text_meltdownDES = "發電機溫度超出了可承受的極限(4000°C)！整座\n設施發生了一場巨大的爆炸，並且摧毀了\n整個設施以及周邊的環境。\n \n提示：記得檢查冷凍液是否開啟，以及是否有管道破裂。"
+    text_freezedown = "凍毀"
+    text_freezedownDES = "由於溫度過低(-4000°C)，一個黑洞產生了，並\n摧毀了整個設施，災害甚至蔓延到整個\n世界。\n“遊戲結束了，兄弟！遊戲結束！”\n \n提示：記得隨時檢查兩個發電機的溫度，並在適當時機關閉或開啟冷凍液。"
+    text_blackout = "設施停電"
+    text_blackoutDES = "由於發電機的能源用盡，沒有能量繼續泵動顏料，\n設施無法再繼續產生電力，遊戲結束。\n \n提示：記得及時維修運輸燃料的管道！"
